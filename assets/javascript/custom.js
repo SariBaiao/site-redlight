1a9843513fc1c219220541b2fb6dbe72333c1926
renato <renatovaz57+git@gmail.com>
2017-06-04 17:01:08 +0100

    apha-wip

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,51 +1,29 @@
  new WOW().init();
 $(document).ready(function(){
 
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 5;
-  var navbarHeight = $('header').outerHeight();
-
-  $(window).scroll(function(event){
-      didScroll = true;
-  });
-
-  setInterval(function() {
-      if (didScroll) {
-          hasScrolled();
-          didScroll = false;
-      }
-  }, 250);
-
-  function hasScrolled() {
-      var st = $(this).scrollTop();
-
-      // Make sure they scroll more than delta
-      if(Math.abs(lastScrollTop - st) <= delta)
-          return;
-
-      // If they scrolled down and are past the navbar, add class .nav-up.
-      // This is necessary so you never see what is "behind" the navbar.
-      if (st > lastScrollTop && st > navbarHeight){
-          // Scroll Down
-          $('header').removeClass('nav-down').addClass('nav-up');
-      } else {
-          // Scroll Up
-          if(st + $(window).height() < $(document).height()) {
-              $('header').removeClass('nav-up').addClass('nav-down');
-              $('header').removeClass('invert').addClass('from-scroll');
-          }
-      }
-
-      lastScrollTop = st;
-  }
-
   $('.burger').on('click', function(){
     $('header').removeClass('from-scroll').addClass('invert');
     $(this).toggleClass('active');
     $('body').toggleClass('menu-open');
     $('header, .menu').toggleClass('open');
     $('.content').toggleClass('hide');
+  });
+
+
+  $('.comunicacao').on('click', function(){
+    $('html,body').animate({
+       scrollTop: $("#comunicacao").offset().top -150
+    });
+    $('manifesto a.active').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $('.comunicacao').on('click', function(){
+    $('html,body').animate({
+       scrollTop: $("#comunicacao").offset().top -150
+    });
+    $('manifesto a.active').removeClass('active');
+    $(this).addClass('active');
   });
 
 });
